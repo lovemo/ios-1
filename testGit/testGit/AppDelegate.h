0c23d091313cb8d09e88baf2fefc44c0a5d260a4
zerd <zerd.zhong@gmail.com>
2013-08-03 14:50:28 +0800

    test
    test...

---
+++ testGit/testGit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  testGit
+//
+//  Created by zerd on 13-8-3.
+//  Copyright (c) 2013年 zerd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
